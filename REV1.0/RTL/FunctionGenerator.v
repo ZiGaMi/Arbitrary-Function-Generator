139fce2291df1825a60eac6dec223f58bc804e30
unknown <ziga.miklosic@gmail.com>
2019-07-23 21:58:26 +0200

    modified:   ../DAC.bak 	modified:   ../DAC.sch 	modified:   ../FPGA.bak 	modified:   ../FPGA.sch 	new file:   arbitrary_func_gen-F_Cu.pdf 	modified:   ../MCU.bak 	modified:   ../MCU.sch 	modified:   ../Power.bak 	modified:   ../Power.sch 	modified:   ../arbitrary_func_gen-cache.lib 	modified:   ../arbitrary_func_gen.bak 	modified:   ../arbitrary_func_gen.kicad_pcb 	modified:   ../arbitrary_func_gen.pro 	../arbitrary_func_gen.kicad_pcb-bak 	../arbitrary_func_gen.net

---
+++ REV1.0/RTL/FunctionGenerator.v
@@ -0,0 +1,51 @@
+`timescale 1ns / 1ns
+
+
+//
+//		Function Generator TOP Module
+//
+//	Author:		Ziga M 
+//	Date: 		20.07.2019
+//
+module FunctionGenerator(
+	sys_clk_i,
+	sys_rst_i,
+	led_o,
+	dac_o
+);
+
+// System clock
+input sys_clk_i;
+
+// System reset
+input sys_rst_i;
+
+// Led
+output [7:0] led_o;
+
+// DAC
+output [11:0] dac_o;
+
+// Counter
+reg [7:0] cnt;
+
+reg [11:0] dac_cnt;
+
+assign dac_o = dac_cnt;
+assign led_o = dac_cnt[7:0];
+
+always @ (posedge sys_clk_i)
+	begin
+		if ( sys_rst_i ) begin
+			cnt <= 12'h0;
+		end
+		else if ( cnt == 8'd25 ) begin
+			cnt <= 12'h0;
+			dac_cnt <= dac_cnt + 1'b1;
+		end
+		else begin
+			cnt <= cnt + 1'b1;
+		end
+	end
+
+endmodule // module: FunctionGenerator
