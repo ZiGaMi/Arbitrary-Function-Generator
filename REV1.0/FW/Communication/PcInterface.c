b0311dd5e854f5f3d626028bd4e5c639fa66a45e
unknown <ziga.miklosic@gmail.com>
2019-08-10 21:20:47 +0200

    new file:   Communication/PcInterface.c 	new file:   Communication/PcInterface.h 	new file:   Drivers/UartDrv.c 	new file:   Drivers/UartDrv.h

---
+++ REV1.0/FW/Communication/PcInterface.c
@@ -0,0 +1,74 @@
+/*
+ * PcInterface.c
+ *
+ *  Created on: 10. avg. 2019
+ *      Author: ZigaM
+ */
+
+
+#include "Communication/PcInterface.h"
+
+
+// Make copy of reception buffer
+static void bufferCopy(uint8_t *dest, uint8_t *src, uint8_t size){
+	for ( uint8_t i = 0u; i < size; i++ ){
+		*( dest + i ) = *( src + i );
+	}
+}
+
+
+// Find header
+// Returns index position at start of header
+static uint8_t getHeaderIndex(uint8_t *buf, uint8_t b_size, uint8_t *header, uint8_t h_size){
+
+	uint8_t h_pos = ( b_size + h_size );
+	uint8_t h_idx = 0u;
+	for ( uint8_t i = 0; i < ( b_size + h_size ); i++ ){
+		//if ( *( buf + ( i % b_size )) == *( header + ( h_idx % h_size ))){
+		if ( *( buf + ( i % b_size )) == *( header + h_idx )){
+			h_idx++;
+			if ( h_idx == h_size ){
+				h_pos = (( i + 1u ) % b_size );
+				break;
+			}
+		}
+		else{
+			h_idx = 0u;
+		}
+	}
+	return h_pos;
+}
+
+
+// Parse data
+// Arguments: input buffer, size of input buffer
+uint8_t *PcInterfaceParseData(uint8_t *rx_buffer, uint8_t size){
+
+	static uint8_t data[UART_RX_BUF_SIZE];
+	uint8_t rx_working_buffer[UART_RX_BUF_SIZE];
+
+	// Make a working copy
+	bufferCopy((uint8_t*) &rx_working_buffer, (uint8_t*) rx_buffer, size);
+
+	// Get header index
+	uint8_t len_idx = getHeaderIndex((uint8_t*) rx_working_buffer, size, PROTOCOL_HEADER, PROTOCOL_HEADER_size);
+
+	// Header detected
+	if ( len_idx != size ){
+
+		// Get data length
+		// Including also length itself
+		uint8_t len = ( rx_working_buffer[len_idx] + 1u );
+
+		// Get data
+		uint8_t idx = 0u;
+		while ( len-- ){
+			data[( idx % size )] = rx_working_buffer[(( idx + len_idx ) % size )];
+			idx++;
+		}
+		return (uint8_t*) ( &data );
+	}
+	else{
+		return NULL;
+	}
+}
---
+++ REV1.0/FW/Drivers/UartDrv.c
@@ -0,0 +1,128 @@
+/*
+ * UartDrv.c
+ *
+ *  Created on: 10. avg. 2019
+ *      Author: ZigaM
+ */
+
+
+#include "Drivers/UartDrv.h"
+
+
+// Initialize uart
+void UartInit(){
+
+	// Enable GPIOA clock
+	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
+
+	// Enable UART1 clock
+	RCC -> APB2ENR |= RCC_APB2ENR_USART1EN;
+
+	// Alternative function
+	UART_port -> MODER &= ~(( 0x03u << ( 2ul * UART_RX_bp )) | ( 0x03u << ( 2ul * UART_TX_bp )));
+	UART_port -> MODER |=  (( 0x02u << ( 2ul * UART_RX_bp )) | ( 0x02u << ( 2ul * UART_TX_bp )));
+
+	// High speed
+	UART_port -> OSPEEDR |= (( 0x03u << ( 2ul * UART_RX_bp )) | ( 0x03u << ( 2ul * UART_TX_bp )));
+
+	// Alternative function
+	UART_port -> AFR[0] &= ~( 0x0F << ( 4ul * UART_TX_bp ));
+	UART_port -> AFR[0] |=  ( UART_TX_afnum << ( 4ul * UART_TX_bp ));
+
+	UART_port -> AFR[0] &= ~( 0x0F << ( 4ul * UART_RX_bp ));
+	UART_port -> AFR[0] |=  ( UART_RX_afnum << ( 4ul * UART_RX_bp ));
+
+	// Disable uart
+	UART -> CR1 &= ~ ( USART_CR1_UE );
+
+	// Enable Rx & Tx, enable Rx interrupt
+	UART -> CR1 |= ( USART_CR1_RE |	USART_CR1_TE );
+
+	// Baudrate
+	UART -> BRR = ( uint32_t ) ( SYS_CLK_FREQ / UART_BAUDRATE );
+
+	// Enable DMA on receiver and transmitter
+	UART -> CR3 |= ( USART_CR3_DMAR | USART_CR3_DMAT );
+
+	// Enable uart
+	UART -> CR1 |= USART_CR1_UE;
+}
+
+
+// Initialize uart1 DMA
+void UartDmaInit(uint8_t *rx_buffer){
+
+	//
+	// 	Configure URAT RX DMA Stream
+	//
+	//  DMA1, Stream1, channel 3
+	//
+
+	// Make sure that UART RX Stream aren't remapped
+	SYSCFG -> CFGR1 &= ~( SYSCFG_CFGR1_USART1RX_DMA_RMP );
+
+	// Enable DMA clock
+	RCC -> AHBENR |= RCC_AHBENR_DMA1EN;
+
+	// Disable DMA channel
+	DMA1_Channel3 -> CCR &= ~( DMA_CCR_EN );
+
+
+	// Medium priority
+	// Memory increment mode
+	// Circular mode
+	// Read from periphery
+	DMA1_Channel3 -> CCR |= ( DMA_CCR_PL_0 | DMA_CCR_MINC | DMA_CCR_CIRC);
+
+	// Number of data to transfer
+	DMA1_Channel3 -> CNDTR = ( uint16_t ) ( UART_RX_BUF_SIZE );
+
+	// Peripheral address to read from
+	DMA1_Channel3 -> CPAR = ( uint32_t ) &( USART1 -> RDR );
+
+	// Memory address to write to
+	DMA1_Channel3 -> CMAR = ( uint32_t ) ( rx_buffer );
+
+	// Enable channel
+	DMA1_Channel3 -> CCR |= ( DMA_CCR_EN );
+
+
+
+	//
+	// 	Configure UART TX DMA Stream
+	//
+	//  DMA1, Stream1, channel 2
+	//
+
+	// Make sure that UART RX Stream aren't remapped
+	SYSCFG -> CFGR1 &= ~( SYSCFG_CFGR1_USART1TX_DMA_RMP );
+
+	// Disable DMA channel
+	DMA1_Channel2 -> CCR &= ~( DMA_CCR_EN );
+
+	// Medium priority
+	// Memory increment mode
+	// Read from memory
+	DMA1_Channel2 -> CCR |= ( DMA_CCR_PL_0 | DMA_CCR_MINC | DMA_CCR_DIR );
+
+	// Peripheral address to read from
+	DMA1_Channel2 -> CPAR = ( uint32_t ) &( USART1 -> TDR );
+}
+
+
+
+// Send packet
+void UartSendString(uint8_t *tx_buf, uint32_t size){
+
+	// Disable DMA channel
+	DMA1_Channel2 -> CCR &= ~( DMA_CCR_EN );
+
+	// Number of data to transmit
+	DMA1_Channel2 -> CNDTR = ( uint32_t ) ( size );
+
+	// Read from
+	DMA1_Channel2 -> CMAR = ( uint32_t ) ( tx_buf );
+
+	// Enable channel
+	DMA1_Channel2 -> CCR |= ( DMA_CCR_EN );
+}
