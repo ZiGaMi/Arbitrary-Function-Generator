7acfddbac95fa0d6f10c98b2fe8bab7f8ff1d3af
unknown <ziga.miklosic@gmail.com>
2019-08-10 11:11:17 +0200

    new file:   REV1.0/SW/FunctionGeneratorDefines.py

---
+++ REV1.0/SW/FunctionGeneratorDefines.py
@@ -0,0 +1,32 @@
+# # #
+# # #		Function Generator Defines
+# # #
+# # #	Author:		Ziga M 
+# # #	Date: 		10.08.2019
+# # #
+
+
+# # # Defines 
+class FunctionGeneratorDefines:
+
+
+	# # # COM Port defines
+	
+	# Serial name
+	COM_SERIAL_NAME = "FUNGEN"
+ 
+	# Baudrate
+	COM_BAUDRATE	= 115200
+	
+	# Timeout
+	COM_TIMEOUT		= 0.100	# s
+	
+	
+	
+	# # # Protocol defines
+	
+	# Header
+	PROT_HEADER		= "\xAA\x55"
+	
+	# 
+	
